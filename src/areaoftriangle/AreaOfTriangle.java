3d91bee4aff5d25c9ed72a088294dbbbfc33ed05
Callum MacLeod <43344986+Xapity@users.noreply.github.com>
2020-02-20 12:54:45 -0500

    Update AreaOfTriangle.java

--- src/areaoftriangle/AreaOfTriangle.java
+++ src/areaoftriangle/AreaOfTriangle.java
@@ -17,11 +17,15 @@ public class AreaOfTriangle {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //set up scanner
         Scanner sn = new Scanner(System.in);
+        //menu instructions
         System.out.println("We're gonna calculate some traingle areas!"
                 + "\nTo use Heron's Law enter 1 (if you have all the side lengths) or,"
                 + "\nto use trig enter 2(if you have two sides and the angle opposite the last side)!");
+        //pick decision
         int dec = sn.nextInt();
+        //heron's law set up
         if (dec == 1) {
             System.out.println("We're gonna be using Heron's Law awesome choice!"
                     + "\nPlease enter the three side lengths:");
@@ -32,7 +36,9 @@ public class AreaOfTriangle {
             double s = (a + b + c) / 2;
 
             double area = (Math.sqrt(s * (s - a) * (s - b) * (s - c)));
+            //print answer
             System.out.println("The area of the triangle is: " + area + "units");
+            //trig set up
         } else {
             System.out.println("We're gonna be using Trig awesome choice!"
                     + "\nPlease enter the two side lengths and then the angle(in dgrees):");
@@ -42,8 +48,8 @@ public class AreaOfTriangle {
             double C = Math.toRadians(C1);
             
             double area = (a + b + Math.sin(C)) / 2;
+            // print answer
             System.out.println("The area of the triangle is: " + area + "units");
-
         }
     }
 
